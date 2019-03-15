8efc4cc47d81fdad34de53285b82da6c60a94402
will-o-the-wisp <kli18@stuy.edu>
2019-03-15 10:42:37 -0400

    created mergesort skeleton

---
+++ Merge.java
@@ -0,0 +1,42 @@
+public class Merge{
+
+  public static void main(String[]args){
+    System.out.println("Size\t\tMax Value\tmerge/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          mergesort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
+    }
+  }
+  public static void mergesort(int[] data){
+  }
+  private static void mergesortH(int[] data, int lo, int hi){
+    
+  }
+}
